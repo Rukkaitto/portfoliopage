4c2d800b322814dbe292d32b5c2f678c010d83ee
Lucas Goudin <goudinlucas@gmail.com>
2020-09-01 16:19:16 +0200

    Added airmap

---
+++ airmap/precache-manifest.d9b36649da082bd98a3638e0dca9ac85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70d871289720f005b40a38625380c06e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b05bfea688723d4ccc2",
+    "url": "/static/css/main.2f3c63a7.chunk.css"
+  },
+  {
+    "revision": "a3fc97ef01445cc7916d",
+    "url": "/static/js/2.278aed9b.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/static/js/2.278aed9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b05bfea688723d4ccc2",
+    "url": "/static/js/main.39db5062.chunk.js"
+  },
+  {
+    "revision": "823c6b691f7086d3dc52",
+    "url": "/static/js/runtime-main.d633236f.js"
+  },
+  {
+    "revision": "61c188ccdd9ef9216c64da8e6a27dae0",
+    "url": "/static/media/loading.61c188cc.gif"
+  }
+]);
